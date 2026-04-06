6aa10b40f61b277de489579b9744acd7306ab3b6
wanilalits <wanilalits@gmail.com>
2026-04-06 14:42:24 +0530

    #

--- Backend/src/modules/sensor/sensor.model.ts
+++ Backend/src/modules/sensor/sensor.model.ts
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema({
   deviceId: String,
   humidity: Number,
+ tempAHT : Number,
+ tempBM:Number,
+    mx:Number,
+    my:Number,
+    mz:Number,
   time: {
     type: Date,
     default: Date.now,
@@ -11,3 +16,11 @@ const schema = new mongoose.Schema({
 
 export default mongoose.model("sensorsdatas", schema);
 //(collection name, schem name)
+
+/*  "deviceId":"D1",
+    "humidity":"11",
+    "tempAHT":"34",
+    "tempBM":"87",
+    "mx":"52",
+    "my":"45",
+    "mz":"52" */
