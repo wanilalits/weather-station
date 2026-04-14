40e280e86e92afff62e619fb84ae0e3b57713bea
wanilalits <wanilalits@gmail.com>
2026-04-14 16:41:19 +0530

    #

--- Backend/src/corn/telegram.cron.ts
+++ Backend/src/corn/telegram.cron.ts
@@ -2,7 +2,7 @@ import cron from "node-cron";
 import { sendTelegramMessage } from "../modules/telegram/telegram.service";
 import {latestData} from  "../websocket/wsServer"
 export const startTelegramCron = () => {
- cron.schedule("*/1 * * * *", async () => {
+ cron.schedule("*/15 * * * *", async () => {
   //cron.schedule("*/1 * * * * *", async () => {
     //console.log(latestData); 
 if (latestData.length > 0) {
