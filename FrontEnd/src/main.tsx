429f00e0c21ad09ed4bbb72590caf29c05d9c71a
wanilalits <wanilalits@gmail.com>
2026-05-05 19:04:42 +0530

    #

--- FrontEnd/src/main.tsx
+++ FrontEnd/src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+//import React from "react";
 import ReactDOM from "react-dom/client";
 import App from './App.tsx'
 import './index.css'
 import "./index.css";
 
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+//import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
 import { store } from "./features/store.ts"; // ✅ IMPORTANT
 import { BrowserRouter } from 'react-router-dom'
