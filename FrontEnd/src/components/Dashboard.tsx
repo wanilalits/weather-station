48d15d2cd330a015a36afb70c3f932246739bb77
wanilalits <wanilalits@gmail.com>
2026-05-08 18:36:07 +0530

    #

--- FrontEnd/src/components/Dashboard.tsx
+++ FrontEnd/src/components/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef  } from 'react';
+import { useEffect  } from 'react';
 import React from 'react';
 import Card from './Card';
 import { useWebSocket } from '../hooks/useWebSocket';
