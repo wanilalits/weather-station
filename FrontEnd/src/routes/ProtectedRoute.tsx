4d8631e380eadf1467e94c48ac7ec166fab3ccfa
wanilalits <wanilalits@gmail.com>
2026-07-08 18:53:53 +0530

    #

--- FrontEnd/src/routes/ProtectedRoute.tsx
+++ FrontEnd/src/routes/ProtectedRoute.tsx
@@ -1,6 +1,5 @@
 import { Navigate } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { tokenVerification} from '../services/tokenVerification'
+
 
 interface Props {
   children: React.ReactNode;
